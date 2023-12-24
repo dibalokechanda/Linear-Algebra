aea81b6dc04e2188ab3fa37f4e55a4611f2f9d23
Dibaloke Chanda <dchanda@mscsnet.mu.edu>
2023-12-24 04:34:47 -0600

    Quartz sync: Dec 24, 2023, 4:34 AM

---
+++ content/index.md
@@ -0,0 +1,10 @@
+---
+title: Linear Algebra
+draft: false
+date: 2023-12-23
+---
+
+This is a repository that contains my personal lecture notes for Linear Algebra.
+To see all the topics use the navigation bar on the left.
+
+
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "📚 Linear Algebra Lecture Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dibaloke.github.io/Linear-Algebra",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
